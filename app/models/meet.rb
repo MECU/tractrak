73b1f2e18ea1066eb2d3fe6e55015b18ddaf5b69
Michael Hoppes <michael.hoppes@checkr.com>
2022-04-27 20:48:02 -0600

    fix QR and PDF generator; footer background

--- app/models/meet.rb
+++ app/models/meet.rb
@@ -17,13 +17,13 @@ class Meet < ApplicationRecord
     sch && evt && ppl && paid?
   end
 
-  def qr(text)
+  def qr(text, size=10)
     require 'barby'
     require 'barby/barcode'
     require 'barby/barcode/qr_code'
     require 'barby/outputter/png_outputter'
-    barcode = Barby::QrCode.new(text, level: :q, size: 9)
-    base64_output = Base64.encode64(barcode.to_png({ xdim: 5 }))
+    barcode = Barby::QrCode.new("https://tractrak.com/#{text}", level: :q, size: 9)
+    base64_output = Base64.encode64(barcode.to_png({ xdim: size }))
     "data:image/png;base64,#{Rack::Utils.escape(base64_output)}"
   end
 
