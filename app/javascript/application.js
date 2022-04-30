528fc7a0bb02bff08fbc07cab27f4ad51a2e8bf7
Michael Hoppes <michael.hoppes@checkr.com>
2022-04-29 21:08:00 -0600

    remove import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,5 @@ import "@rails/activestorage"
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "./channels"
-import '@fortawesome/fontawesome-free/js/all.js'
 import "./add_luxon"
 import * as bootstrap from "bootstrap"
