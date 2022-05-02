ab53b140c46eb58e118e889dc2fa9b3c0bd1102a
Michael Hoppes <michael.hoppes@checkr.com>
2022-05-02 08:11:56 -0600

    don't see for test env; factory all the things

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe DashboardController do
   let(:meet) { create(:meet, ppl: true) }
+  let!(:state) { create(:state, id: 6) }
   let(:user) { create(:user) }
 
   before do
