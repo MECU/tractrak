9a6f5ec97ccf2bd123ec1400891c2c410daab60e
Michael Hoppes <michael.hoppes@checkr.com>
2022-07-22 11:48:21 -0600

    logtail config, update nokogiri

---
+++ config/initializers/logtail.rb
@@ -0,0 +1,6 @@
+if ENV['LOGTAIL_SKIP_LOGS'].blank? && !Rails.env.test?
+  http_device = Logtail::LogDevices::HTTP.new('1HbrVKTmoMA8j2z7Veje34vY')
+  Rails.logger = Logtail::Logger.new(http_device)
+else
+  Rails.logger = Logtail::Logger.new(STDOUT)
+end
