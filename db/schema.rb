24e00f9fab3bf4c99bf357451ada8ca60efbf821
Michael Hoppes <michael.hoppes@checkr.com>
2022-07-23 06:45:19 -0600

    add meet_end_date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_22_111234) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_23_111234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_22_111234) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "milesplit_id"
+    t.datetime "meet_end_date"
     t.index ["discarded_at"], name: "index_meets_on_discarded_at"
     t.index ["meet_key"], name: "index_meets_on_meet_key", unique: true
     t.index ["owner_id"], name: "index_meets_on_owner_id"
