f25ecbf51218d403dc0877cad785437c66cfe6fe
Michael Hoppes <michael.hoppes@checkr.com>
2022-07-22 20:34:46 -0600

    run seed before test

--- spec/factories/stadium_factory.rb
+++ spec/factories/stadium_factory.rb
@@ -4,9 +4,9 @@ FactoryBot.define do
     google_name { Faker.name }
     address { Faker::Address.street_address }
     city { Faker::Address.city }
-    state { State.all.first }
+    state { State.first }
     zip { Faker::Address.zip }
-    country { Country.all.first }
+    country { Country.first }
     lat { Faker::Address.latitude }
     lng { Faker::Address.longitude }
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,5 +33,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.before(:suite) do
+    Rails.application.load_seed
+  end
+
   config.include Devise::Test::ControllerHelpers, type: :controller
 end
