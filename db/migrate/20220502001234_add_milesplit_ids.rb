d3ac4c5dd4a54b2fad3c30bcb7314737dece506b
Michael Hoppes <michael.hoppes@checkr.com>
2023-04-20 00:13:48 -0600

    fix migration

--- db/migrate/20220502001234_add_milesplit_ids.rb
+++ db/migrate/20220502001234_add_milesplit_ids.rb
@@ -2,12 +2,10 @@ class AddMilesplitIds < ActiveRecord::Migration[7.0]
   def up
     add_column :athletes, :milesplit_id, :integer
     add_column :teams, :milesplit_id, :integer
-    add_column :meets, :milesplit_id, :integer
   end
 
   def down
     remove_column :athletes, :milesplit_id
     remove_column :teams, :milesplit_id
-    remove_column :meets, :milesplit_id
   end
 end
