c86277f116ae474722a003363ae715f0ce06a028
Michael Hoppes <michael.hoppes@checkr.com>
2023-04-22 00:26:20 -0600

    Add search

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import SearchFormController from "./search_form_controller"
+application.register("search-form", SearchFormController)
